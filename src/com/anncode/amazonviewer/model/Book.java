2e005d93ed2948f32e4b5c6ab325a48607555f97
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-04 22:15:45 -0500

    v04jul22.2215 - clases anidadas aplicados al proyecto

--- src/com/anncode/amazonviewer/model/Book.java
+++ src/com/anncode/amazonviewer/model/Book.java
@@ -3,18 +3,33 @@ package com.anncode.amazonviewer.model;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.anncode.util.AmazonUtil;
 
 public class Book extends Publication implements IVisualizable {
 	private int id;
 	private String isbn;
 	private boolean readed;
 	private int timeReaded;
+	private List<Page> pages;
 	
 	
-	public Book(String title, Date edititionDate, String editorial, String[] authors) {
+	public List<Page> getPages() {
+		return pages;
+	}
+
+
+	public void setPages(List<Page> pages) {
+		this.pages = pages;
+	}
+
+
+	public Book(String title, Date edititionDate, String editorial, String[] authors, List<Page> pages) {
 		super(title, edititionDate, editorial);
 		// TODO Auto-generated constructor stub
 		setAuthors(authors);
+		this.pages = pages;
 	}
 
 
@@ -101,8 +116,17 @@ public class Book extends Publication implements IVisualizable {
 		for (int i = 0; i < 3; i++) {
 			authors[i] = "author "+i;
 		}
+
+		List<Page> pages = new ArrayList<>();
+		int pagina = 0;
+		for (int i = 0; i < 3; i++) {
+			pagina = i+1;
+			pages.add(new Book.Page(pagina, "El contenido de la página: " + pagina));
+		}
+		
+
 		for (int i = 1; i <= 5; i++) {
-			books.add(new Book("Book " + i, new Date(), "editorial " + i, authors));
+			books.add(new Book("Book " + i, new Date(), "editorial " + i, authors, pages));
 		}
 		
 		return books;
@@ -111,8 +135,32 @@ public class Book extends Publication implements IVisualizable {
 	public void view(){
 		setReaded(true);
 		Date dateI = startToSee(new Date());
+
+		int i = 0;
+		do{
+			System.out.println(">>>>>>>>>>");
+			System.out.println("Page: " + getPages().get(i).getNumberOfPage());
+			System.out.println(getPages().get(i).getContent());
+			System.out.println(">>>>>>>>>>");
+			if (i != 0){
+				System.out.println("1. Regresar página");
+			}
+			System.out.println("2. Siguiente página");
+			System.out.println("0. Cerrar libro");
+			System.out.println("");
+			int response = AmazonUtil.validateUserResponseMenu(0, 2);
+			if (response == 2) {
+				i++;
+			}else if (response == 1){
+				i--;
+			}else if (response == 0){
+				break;
+			}
+
+		}while(i < getPages().size() );
 		
-		for (int i = 0; i < 10; i++) {
+
+		for (int ii = 0; ii < 10; ii++) {
 			System.out.println("..........");
 		}
 		
@@ -122,6 +170,36 @@ public class Book extends Publication implements IVisualizable {
 		System.out.println("Leíste: " + toString());
 		System.out.println("Por: " + getTimeReaded() + " milisegundos");
 
+	}
+
+	public static class Page{
+		private int id;
+		private int numberOfPage;
+		private String content;
+
+		public Page(int numberOfPage, String content) {
+			this.numberOfPage = numberOfPage;
+			this.content = content;
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public int getNumberOfPage() {
+			return numberOfPage;
+		}
+		public void setNumberOfPage(int numberOfPage) {
+			this.numberOfPage = numberOfPage;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+		
 	}
 	
 }
